d3bf6ef4244ffaf3066153a7f14c59083a17d721
neel <neelkandlikar@gmail.com>
2020-05-16 12:51:55 -0700

    scraped odm into tsv

--- check_csv.py
+++ check_csv.py
@@ -10,7 +10,7 @@ with open('odm/organizations.csv', 'r') as odm:
 
 def write_to_output(file_name, to_write):
     with open(file_name, mode='a') as output_file:
-        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(output_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(to_write)
         print("Written to " + str(file_name) + " " + str(company_name))
         output_file.close()
@@ -25,10 +25,10 @@ with open('updatedDataCopy.tsv', 'r') as data:
             odm_reader = csv.reader(odm)
             for row_odm in odm_reader:
                 if row_odm[1] == company_name:
-                    write_to_output("in_odm.csv", row_odm)
+                    write_to_output("in_odm.tsv", row_odm)
                 if row_odm == last_line:
                     print("written to not")
-                    write_to_output("not_in_odm.csv", row_odm)
+                    write_to_output("not_in_odm.tsv", row_odm)
 
 
 
