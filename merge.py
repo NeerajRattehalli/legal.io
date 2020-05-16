5e459754d9f12f75030c82087c2fe06ad924a8f3
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-05-15 17:16:37 -0700

    fixed merging issues and added comments

--- merge.py
+++ merge.py
@@ -1,7 +1,38 @@
 compDict = {}
-count = 0
 
+# takes the two pieces of data and merges them by relevant category
+def merge(name, ourData, cbData):
+    goodData = {}
+    for item in ourData:
+        validData  = check(name, item, ourData[item], cbData[item])
+        goodData[item] = validData
+    return goodData
+
+# a diciotnary containing all data points that should be checked and validted manually
+thingsToManuallyCheck = {}
+
+# determine what is the right value and which to go with
+def check(name, field, ourItem, cbItem):
+    if (ourItem == "0" and cbItem == "0"):
+        if not thingsToManuallyCheck[name]:
+            thingsToManuallyCheck[name] = []
+        thingsToManuallyCheck[name].append(field)
+        return "0"
+    if (ourItem == "0"):
+         return cbItem
+    if (cbItem == "0"):
+        return ourItem
+    if (ourItem in cbItem) or (cbItem in ourItem):
+        return ourItem
+    else:
+        if name not in thingsToManuallyCheck.key():
+            thingsToManuallyCheck[name] = []
+        thingsToManuallyCheck[name].append(field)
+    return "0"
+
+#this reads the data from our 2000+ db of items and creates an object like structure containing all the details
 with open('final_updated.tsv','r') as in_file:
+    count = 0
     for line in in_file:
         count = count + 1
         if (count>1):
@@ -21,53 +52,34 @@ with open('final_updated.tsv','r') as in_file:
                                 "linkedin_url": "0",
                                 "facebook_url": "0"}
 
+
+#this reads from a crunchbase database and gathers any additional information
 with open('in_odm.csv','r') as in_file:
+    count = 0
     for line in in_file:
-        uuid,name,compType,primary_role,cb_url,domain,homepage_url,logo_url,facebook_url,twitter_url,linkedin_url,combined_stock_symbols,city,region,country_code,short_description =  line.split(",")
+        uuid,name,compType,primary_role,cb_url,domain,homepage_url,logo_url,height,width,address,facebook_url,twitter_url,linkedin_url,combined_stock_symbols,city,region,country_code =  line.split(",")[:18]
         name = name.lower()
-        ourCSVDetails = compDict[name]
 
-        cbDBDetails = {"date": "0", 
-                        "hq": city, 
-                        "category": "0", 
-                        "audience": "0", 
-                        "model": "0", 
-                        "description": "0", 
-                        "main_url": homepage_url, 
-                        "twitter_url": twitter_url, 
-                        "angellist_url": "0", 
-                        "crunchbase_url": cb_url, 
-                        "tags": "0", 
-                        "linkedin_url": linkedin_url,
-                        "facebook_url": facebook_url}
+        if name in compDict.keys():
+            ourCSVDetails = compDict[name]
 
-        merged = merge(name, ourCSVDetails, cbDBDetails)
-        compDict[name] = merged
-        
-def merge(name, ourData, cbData):
-    goodData = {}
-    for item in ourData:
-        validData  = check(name, item, ourData[item], cbData[item])
-        goodData[item] = validData
-    return goodData
+            cbDBDetails = {"date": "0", 
+                            "hq": city, 
+                            "category": "0", 
+                            "audience": "0", 
+                            "model": "0", 
+                            "description": "0", 
+                            "main_url": homepage_url, 
+                            "twitter_url": twitter_url, 
+                            "angellist_url": "0", 
+                            "crunchbase_url": cb_url, 
+                            "tags": "0", 
+                            "linkedin_url": linkedin_url,
+                            "facebook_url": facebook_url}
+            # merged = merge(name, ourCSVDetails, cbDBDetails)
+            # compDict[name] = merged
+        else:
+            thingsToManuallyCheck[name] = "everything"
 
-thingsToManuallyCheck = {}
 
-def check(name, field, ourItem, cbItem):
-    if (ourItem == "0" and cbItem == "0"):
-        if not thingsToManuallyCheck[name]:
-            thingsToManuallyCheck[name] = []
-        thingsToManuallyCheck[name].append(field)
-        return "0"
-    if (ourItem == "0"):
-         return cbItem
-    if (cbItem == "0"):
-        return ourItem
-    if (ourItem in cbItem) or (cbItem in ourItem):
-        return ourItem
-    else:
-        if not thingsToManuallyCheck[name]:
-            thingsToManuallyCheck[name] = []
-        thingsToManuallyCheck[name].append(field)
-    return "0"
 
