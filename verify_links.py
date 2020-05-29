114e8144a1d443fc57d764f97ab6b8bc4c029856
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-05-29 01:19:50 -0700

    additional checks fully function verification code

--- verify_links.py
+++ verify_links.py
@@ -9,8 +9,30 @@ def check(urlString, compName, url):
     url = url.lower()
 
     # Case 1: Company Name has no spaces
+    
+        
     if " " not in compName:
+        
+        if "." in compName:
+            # Splits companies names
+            compNameWord = compName.split(".")
 
+            # prelim parsing
+            for word in compNameWord:
+
+                # lowers each element
+                lowerWord = word.lower()
+                
+                #checks that it's not a useless word
+                if "inc" in lowerWord or "llc" in lowerWord or "p.c." in lowerWord:
+                    continue
+                else:
+                    #checks that each word is part of url
+                    if lowerWord not in url:
+                        return False
+
+        # all conditions work for len > 1
+        return True
         # splits at uppercase letters
         splitName = re.findall('[A-Z][^A-Z]*', compName)
 
@@ -68,6 +90,11 @@ def check(urlString, compName, url):
 # print(check("twitter", "DISCO", "twitter"))
 # print(check("facebook", "MyCourthouse", " https://www.facebook.com/courthouseclubs/videos/on-the-second-day-of-christmas-my-trainer-brought-to-me-push-ups-how-many-can-yo/569793773433336/"))
 # print(check("twitter", "PayMyTrustee", " https://twitter.com/paymytrustee"))
+
+print(check("twitter", "Free Law Project", ' https://twitter.com/FreeLawProject?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'))
+print(check("twitter", "PSS Systems", " https://twitter.com/pss_systems?lang=en"))
+print(check("twitter", "Legal.io", " https://twitter.com/Legal_io?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"))
+print(check("twitter", "Legal Passage", " https://help.twitter.com/en/rules-and-policies/twitter-legal-faqs"))
 
 # with open('compananiesWithSocialsNoDups.csv','r') as in_file:
 #     for line in in_file:
