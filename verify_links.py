5cc8d8741cf02089cc9881adc2ef823768e4349f
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-05-29 01:07:28 -0700

    added checking for multi word comp name

--- verify_links.py
+++ verify_links.py
@@ -4,15 +4,15 @@ def check(urlString, compName, url):
     # checks if the required string is in the url link itself
     if urlString not in url:
         return False
-    
+
+    #lowercase url
+    url = url.lower()
+
     # Case 1: Company Name has no spaces
     if " " not in compName:
 
         # splits at uppercase letters
         splitName = re.findall('[A-Z][^A-Z]*', compName)
-
-        #lowercase url
-        url = url.lower()
 
         # edge cases that need direct checking
         if len(splitName) == 1:
@@ -38,15 +38,36 @@ def check(urlString, compName, url):
                 prevIndex = index   
         # passed all the tests for len = 1 
         return True
+    else:
 
+        # Splits companies names
+        compNameWord = compName.split(" ")
+
+        # prelim parsing
+        for word in compNameWord:
+
+            # lowers each element
+            lowerWord = word.lower()
+            
+            #checks that it's not a useless word
+            if "inc" in lowerWord or "llc" in lowerWord or "p.c." in lowerWord:
+                continue
+            else:
+                #checks that each word is part of url
+                if lowerWord not in url:
+                    return False
+
+        # all conditions work for len > 1
+        return True
+            
 
 # CHECKS
 
-print(check("facebook", "Chapter 11 Dockets", " https://www.chapter11dockets.com/"))
-print(check("twitter", "Corporize",  " https://twitter.com/corporatize"))
-print(check("twitter", "DISCO", "twitter"))
-print(check("facebook", "MyCourthouse", " https://www.facebook.com/courthouseclubs/videos/on-the-second-day-of-christmas-my-trainer-brought-to-me-push-ups-how-many-can-yo/569793773433336/"))
-print(check("twitter", "PayMyTrustee", " https://twitter.com/paymytrustee"))
+# print(check("facebook", "Chapter 11 Dockets", " https://www.chapter11dockets.com/"))
+# print(check("twitter", "Corporize",  " https://twitter.com/corporatize"))
+# print(check("twitter", "DISCO", "twitter"))
+# print(check("facebook", "MyCourthouse", " https://www.facebook.com/courthouseclubs/videos/on-the-second-day-of-christmas-my-trainer-brought-to-me-push-ups-how-many-can-yo/569793773433336/"))
+# print(check("twitter", "PayMyTrustee", " https://twitter.com/paymytrustee"))
 
 # with open('compananiesWithSocialsNoDups.csv','r') as in_file:
 #     for line in in_file:
