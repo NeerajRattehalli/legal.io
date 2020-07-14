ec4bef63ef29b2b1bb6d4c9591e6495dec359af8
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-07-14 01:48:06 -0700

    added all new final files

--- fixCategory.py
+++ fixCategory.py
@@ -2,11 +2,11 @@ companyMainDict = {}
 
 # Read Input Data
 with open('./final/finalWithStatus/final.tsv','r') as in_file:
+    start = False
     for line in in_file:
-        print(line.split("\t"))
-
-        name, date, hq, category, audience, model, description, homepage_url, twitter_url, angellist_url, cb_url, linkedin_url, facebook_url, tags, status = line.split("\t")
-        companyMainDict[name] = {"date": date, 
+        if (start):
+            name, date, hq, category, audience, model, description, homepage_url, twitter_url, angellist_url, cb_url, linkedin_url, facebook_url, tags, status = line.split("\t")
+            companyMainDict[name] = {"date": date, 
                                     "hq": hq, 
                                     "category": category, 
                                     "audience": audience, 
@@ -20,6 +20,8 @@ with open('./final/finalWithStatus/final.tsv','r') as in_file:
                                     "facebook_url": facebook_url,
                                     "tags": tags,
                                     "status": status}
+                                    
+        start = True
 
 for company in companyMainDict:
     category = companyMainDict[company]["category"].split(" -")[0]
