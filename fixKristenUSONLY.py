94a08c4173a7b4f380c7dafd951542a5c55eb80f
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-08-03 00:52:09 -0700

    Merged All US Only

--- fixKristenUSONLY.py
+++ fixKristenUSONLY.py
@@ -36,20 +36,22 @@ with open('USONLY/Data 7_20_20 - Diversity Working Copy - US Only Order Preserve
             start = True
         else:
             Status, name, date, hq, USbased, category, audience, model, description, main_url, twitter_url, angellist_url, crunchbase_url, linkedin_url, facebook_url, tags, status = line.split("\t")
-            compDictItem = companyMainDict[name]
-            date = companyMainDict[name]["date"]
-            hq = companyMainDict[name]["hq"]
-            category = companyMainDict[name]["category"]
-            audience = companyMainDict[name]["audience"]
-            model = companyMainDict[name]["model"]
-            description = companyMainDict[name]["description"]
-            main_url = companyMainDict[name]["main_url"]
-            linkedin_url = companyMainDict[name]["linkedin_url"]
-            facebook_url = companyMainDict[name]["facebook_url"]
-            tags = companyMainDict[name]["tags"]
-            status = companyMainDict[name]["status"]
+            compDictItem = companyMainDict[name] 
+
+            date = companyMainDict[name]["date"] if (date=="n/a") else date
+            hq = companyMainDict[name]["hq"] if (hq=="n/a") else hq
+            category = companyMainDict[name]["category"] if (category=="n/a") else category
+            audience = companyMainDict[name]["audience"] if (audience=="n/a") else audience
+            model = companyMainDict[name]["model"] if (model=="n/a") else model
+            description = companyMainDict[name]["description"] if (description=="n/a") else description
+            main_url = companyMainDict[name]["main_url"] if (main_url=="n/a") else main_url
+            linkedin_url = companyMainDict[name]["linkedin_url"] if (date=="n/a") else linkedin_url
+            facebook_url = companyMainDict[name]["facebook_url"] if (date=="n/a") else facebook_url
+            tags = companyMainDict[name]["tags"] if (tags=="n/a") else tags
+            status = companyMainDict[name]["status"] if (status=="n/a") else status
 
             finalCompDict[name] = { "Status": Status,
+                                    "name": name,
                                     "date": date, 
                                     "hq": hq, 
                                     "US based?": USbased,
@@ -66,5 +68,33 @@ with open('USONLY/Data 7_20_20 - Diversity Working Copy - US Only Order Preserve
                                     "tags": tags,
                                     "status": status}
 
+# Writing Final Files
+with open('USONLYFINAL/finalV1/USOnly.csv', 'w') as out_file:
+    out_file.write(",".join(list(finalCompDict['AfterIGo'].keys()))+"\n")
+    for compName in finalCompDict:
+        line = ""
+        for description in finalCompDict[compName]:
+            item = finalCompDict[compName][description]
+            if "\n" in item:
+                item = item[:-1]
+            if "," in item:
+                line += ",\"" + item + "\""
+            else:
+                line += "," + item
+            if item == "":
+                line += "n/a"
+        line += "\n"
+        out_file.write(line)
 
-print(len(finalCompDict))
+with open('USONLYFINAL/finalV1/USOnly.tsv', 'w') as out_file:
+    out_file.write("\t".join(list(finalCompDict['AfterIGo'].keys()))+"\n")
+    for compName in finalCompDict:
+        line = ""
+        for description in finalCompDict[compName]:
+            line += finalCompDict[compName][description].replace("\n", "") + "\t" 
+            if finalCompDict[compName][description] == "":
+                line += "n/a"
+
+        line += "\n"
+
+        out_file.write(line)
