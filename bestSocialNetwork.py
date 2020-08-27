7e240c5aeaf696b00c7c107a9898f64c3a213661
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-08-27 13:52:57 -0700

    added best socials

---
+++ bestSocialNetwork.py
@@ -0,0 +1,25 @@
+import csv
+socials = {}
+
+with open("final/ManualFinalV4/final.tsv") as in_file:
+    reader = csv.reader(in_file, delimiter = "\t", quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+    for row in reader:
+        try:
+            subSocials = {"twitter": row[8],
+                         "angellist": row[9],
+                         "crunchbase": row[10],
+                         "linkedin": row[11],
+                         "facebook": row[12] }
+        except IndexError:
+            continue
+
+        for social in subSocials:
+            if social not in socials:
+                socials[social] = 0
+            url = subSocials[social]
+            if "n/a" not in url:
+                socials[social] += 1
+        
+
+print(socials)
+        
